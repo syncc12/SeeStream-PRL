92543d7439564a17a8e58329130fb27a7fb49cb1
Patrick Lyden <syncc12@verizon.net>
2019-09-03 19:18:03 +0000

    Added ability to upload videos.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,13 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  # config.fog_public = false
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-    region: ENV['S3_REGION']
-  }
+  config.storage    = :aws
+  config.aws_bucket = ENV["AWS_BUCKET"]
+  config.aws_acl    = "public-read"
 
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  config.aws_credentials = {
+    access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    region:            ENV['AWS_REGION']
+  }
 end

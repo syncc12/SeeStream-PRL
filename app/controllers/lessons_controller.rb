3e99adb999f1def152df8869c20f8545d01df19e
Patrick Lyden <syncc12@verizon.net>
2019-09-14 02:27:13 +0000

    Added course enrollment requirement to view lessons.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,6 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  #before_action :require_authorized_for_current_lesson, only: [:show]
+  before_action :require_authorized_for_current_lesson, only: [:show]
 
   def show
   end
@@ -12,17 +12,12 @@ class LessonsController < ApplicationController
     @current_lesson ||= Lesson.find(params[:id])
   end
 
-  # def current_enrollment
-    # @current_enrollment ||= Enrollment.find(:user_id)
-  # end
-
-#   def require_authorized_for_current_lesson
-#     if current_enrollment.include?(current_lesson.section.course)
-#       #current_user.enrolled_in?(current_lesson)
-#       #current_lesson.section.course
-#     else
-#       redirect_to '#', alert: 'Course enrollment required before viewing course lessons.'
-#       #render plain: "Unauthorized", status: :unauthorized
-#     end
-#   end
+  def require_authorized_for_current_lesson
+    if current_user.enrolled_in?(current_lesson.section.course)
+      #current_lesson.section.course
+    else
+      redirect_to course_path(current_lesson.section.course), alert: 'Course enrollment required before viewing course lessons.'
+      #render plain: "Unauthorized", status: :unauthorized
+    end
+  end
 end

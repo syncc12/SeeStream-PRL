5f15c80b9f29a3a70848d4afa0482d300840d0eb
Patrick Lyden <syncc12@verizon.net>
2019-09-15 19:17:33 +0000

    Added ability for the instructor to reorder their lessons within each section.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
+//= require jquery
+//= require jquery_ujs
 //= require popper
 //= require tether
 //= require bootstrap-sprockets
 //= require_tree .
+//= require jquery-ui

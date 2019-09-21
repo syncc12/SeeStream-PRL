0e2cd73198db4a4ee0869c25554242b212358fa6
Patrick Lyden <syncc12@verizon.net>
2019-09-21 15:30:23 +0000

    Misc Updates.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,18 @@
 class StaticPagesController < ApplicationController
 
+  # Index Static Page
   def index
   end
 
+  # Privacy Policy Static Page
   def privacy
   end
 
+  # Team Static Page
   def team
   end
 
+  # Careers Static Page
   def career
   end
 

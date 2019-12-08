a1635ae151d5ff28be3a59369c1ec21ac0756186
Patrick Lyden <syncc12@verizon.net>
2019-12-08 20:30:46 +0000

    CORS Update

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module SeeStream
 
     config.middleware.insert_before 0, Rack::Cors do
         allow do
-            origins 'http://localhost:3030'
+            origins ['http://localhost:3030', 'https://portfolio-patricklyden.herokuapp.com', 'http://patrickrlyden.com']
             resource '*', :headers => :any, :methods => [:get, :post, :options]
         end
     end
